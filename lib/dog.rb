eaf691e9d94b0f86bd634916fb9c9abe1858f24c
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-22 01:24:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,6 +25,20 @@ class Dog
     DB[:conn].execute(sql)
   end
   
+  def save
+      if self.id
+    self.update
+  else
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+  end
+
+  
   def self.new_from_db(row)
     dog = self.new(row[0], row[1], row[2])
     dog
