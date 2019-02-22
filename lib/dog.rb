8081b785ccd0e6a521254d1561996320b0b3b8ab
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-22 02:09:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,8 +62,12 @@ class Dog
   end 
   
   def self.new_from_db(row)
-    dog = self.new(row[0], row[1], row[2])
-    dog
+        attributes_hash = {
+      :id => row[0],
+      :name => row[1],
+      :breed => row[2]
+    }
+    self.new(attributes_hash)
   end
   
   def self.find_by_name(name)
