e7ce9dc4ab23d730d1c13875d40f00cd2d0f3e10
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-22 01:10:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,6 +9,20 @@ class Dog
     @breed = breed 
   end
   
-  
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
   
 end
